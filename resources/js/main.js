f364ce3661ef60bd43ceea8df9e4e033a12050c5
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-05-07 17:15:28 +0200

    add smooth scrolling

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,33 @@
+// Vars for fixed nav
 const content = document.querySelector('.section-features');
 const threshold = content.offsetTop - 60;
-
+// Vars for smooth scrolling
+const anchors = document.querySelectorAll('header a');
+// fixed nav logic
 const fixNav = () => {
     if(window.scrollY >= threshold) {
         document.body.classList.add('sticky-nav');
     } else {
         document.body.classList.remove('sticky-nav');
     }
+};
+
+// scrolling logic
+function scrollIt(element) {
+    window.scrollTo({
+        'behavior': 'smooth',
+        'left': 0,
+        'top': element.offsetTop - 60
+    });
 }
+
+//add event listeners
+anchors.forEach( anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        let elScrollTo = document.querySelector(this.getAttribute('href'));
+        scrollIt(elScrollTo);
+    });
+});
 
 window.addEventListener('scroll', fixNav);
